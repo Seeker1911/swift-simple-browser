c2250ff27aa4ffc4f5d8d3501530634200c93686
Michael Mead <michaelmead007@icloud.com>
2016-09-05 16:58:54 -0500

    Simple browser app.

--- simple browser/ViewController.swift
+++ simple browser/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var addressBar: UITextField!
+    @IBOutlet var infoLabel: UILabel!
+    @IBOutlet var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,39 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func didTapGo(sender: AnyObject) {
+        if addressBar.text == "" {
+            return
+        }
+        guard let text:String = addressBar.text else {
+            return
+        }
+        infoLabel.hidden = true
+        if text.rangeOfString(".") != nil {
+            var finalString:String = text.lowercaseString
+            if finalString.rangeOfString("http://") == nil {
+                finalString = "http://\(finalString)"
+            }
+            self.loadURL(finalString)
+        } else {
+            let searchStr = text.stringByReplacingOccurrencesOfString(" ", withString: "+")
+            let finalString = "https://www.google.com/search?q=\(searchStr)"
+            self.loadURL(finalString)
+        }
+    }
+    
+    func loadURL(str:String) {
+        let url = NSURL(string: str)!
+        let request = NSURLRequest(URL: url)
+        webView.loadRequest(request)
+    }
+    
+    @IBAction func goBack(sender: AnyObject) {
+        webView.goBack()
+    }
 
+    @IBAction func goForward(sender: AnyObject) {
+        webView.goForward()
+    }
 }
 
